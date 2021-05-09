d85d780f24e40733d3c3d068d40a4638c4f52816
Danny xyy <673699118@qq.com>
2021-05-09 14:34:09 -0400

    add cu

---
+++ sum_matrix.cu
@@ -0,0 +1,21 @@
+#include <cuda_runtime.h>
+#include <stdio.h>
+#include "cudastart.h"
+
+//CPU对照组，用于对比加速比
+void sumMatrix2DonCPU(float * MatA, float * MatB, float * MatC, int nx, int ny)
+{
+    float* a = MatA;
+    float* b = MatB;
+    float* c = MatC;
+    for (int j = 0; j < ny; j++)
+    {
+        for(int i = 0; i < nx; i++)
+        {
+            c[i] = a[i] + b[i];
+        }
+        c += nx;
+        b += nx;
+        a += nx;
+    }
+}
